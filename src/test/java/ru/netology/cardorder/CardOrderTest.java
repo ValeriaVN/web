8ed2cc913ec769f5fa3681a03f3abce7a451096e
Valeria N <poponuk@gmail.com>
2022-08-30 16:49:22 +0300

    new test

--- src/test/java/ru/netology/cardorder/CardOrderTest.java
+++ src/test/java/ru/netology/cardorder/CardOrderTest.java
@@ -1,5 +1,6 @@
-package ru.netology.CardOrder;
+package ru.netology.cardorder;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
@@ -7,10 +8,15 @@ import static com.codeborne.selenide.Condition.*;
 
 public class CardOrderTest {
 
+    @BeforeEach
+    void Page() {
+        open("http://localhost:9999");
+    }
+
 
     @Test
     public void shouldSubmitRequest(){
-       open("http://localhost:9999");
+
         $("[data-test-id=name] input").setValue("Александр Иванов");
         $("[data-test-id=phone] input").setValue("+74951001010");
         $("[data-test-id=agreement]").click();
@@ -20,8 +26,19 @@ public class CardOrderTest {
    }
 
     @Test
+    public void shouldFieldHyphenName(){
+
+        $("[data-test-id=name] input").setValue("Александр Петров-Водкин");
+        $("[data-test-id=phone] input").setValue("+74951001010");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+
+    }
+
+    @Test
     public void shouldFieldErrorName(){
-        open("http://localhost:9999");
+
         $("[data-test-id=name] input").setValue("Alex Ivanov");
         $("[data-test-id=phone] input").setValue("+74951001010");
         $("[data-test-id=agreement]").click();
@@ -30,9 +47,10 @@ public class CardOrderTest {
 
     }
 
+
     @Test
     public void shouldFieldErrorPhone(){
-        open("http://localhost:9999");
+
         $("[data-test-id=name] input").setValue("Александр Иванов");
         $("[data-test-id=phone] input").setValue("+74951001010000");
         $("[data-test-id=agreement]").click();
@@ -41,14 +59,37 @@ public class CardOrderTest {
 
     }
 
+
     @Test
     public void shouldErrorPersonalData(){
-        open("http://localhost:9999");
+
         $("[data-test-id=name] input").setValue("Александр Иванов");
         $("[data-test-id=phone] input").setValue("+74951001010");
         $("[type=button]").click();
         $("[data-test-id=agreement].input_invalid .checkbox__text").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
 
     }
+
+    @Test
+    public void shouldFieldEmptyName(){
+
+        $("[data-test-id=phone] input").setValue("+74951001010000");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+
+    }
+
+    @Test
+    public void shouldFieldEmptyPhone(){
+
+        $("[data-test-id=name] input").setValue("Александр Иванов");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+
+    }
+
+
 
 }
